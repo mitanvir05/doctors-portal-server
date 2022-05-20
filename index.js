7f94795a670e31ea6203d6aa859585961570c0d6
mitanvir05 <tanvir.cse05@gmail.com>
2022-05-20 13:48:26 +0600

    service update

--- index.js
+++ index.js
@@ -20,12 +20,57 @@ async function run() {
     const serviceCollection = client
       .db("doctors_portal")
       .collection("services");
+    const bookingCollection = client
+      .db("doctors_portal")
+      .collection("bookings");
 
     app.get("/service", async (req, res) => {
       const query = {};
       const cursor = serviceCollection.find(query);
       const services = await cursor.toArray();
       res.send(services);
+    });
+
+    app.get("/available", async (req, res) => {
+      const date = req.query.date || "May 20, 2022";
+      //get all services
+      const services = await serviceCollection.find().toArray();
+      // get the booking of that day
+      const query = { date: date };
+      const bookings = await bookingCollection.find(query).toArray();
+      // for each service ,find bookings for that service
+      services.forEach((service) => {
+        const serviceBookings = bookings.filter(
+          (b) => b.treatment === service.name
+        );
+        const booked = serviceBookings.map((s) => s.slot);
+        const available = service.slots.filter((s) => !booked.includes(s));
+        service.available = available;
+      });
+      res.send(services);
+    });
+    /**
+     * API naming
+     * app.get('/booking')//get all booking services
+     * app.get('/booking:id') //get a specific booking
+     * app.post('booking') // add new booking
+     * app.patch('/booking:id')
+     * app.delete('/booking:id')
+     */
+
+    app.post("/booking", async (req, res) => {
+      const booking = req.body;
+      const query = {
+        treatment: booking.treatment,
+        date: booking.date,
+        patient: booking.patient,
+      };
+      const exists = await bookingCollection.findOne(query);
+      if (exists) {
+        return res.send({ success: false, booking: exists });
+      }
+      const result = await bookingCollection.insertOne(booking);
+      return res.send({ success: true, result });
     });
   } finally {
   }
